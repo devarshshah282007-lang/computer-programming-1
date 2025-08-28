d37d0cd73bba97001f6d638fe39451b508a7bf7a
Devarsh Shah <devarshshah282007@gmail.com>
2025-08-28 22:35:21 +0530

    Create add,multi,sub,div.c

---
+++ add,multi,sub,div.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    float a,b,c;
+    printf("enter two numbers:");
+    scanf("%f%f" ,&a,&b);
+    c=a+b;
+    printf("%.2f+%.2f=%.2f\n" ,a,b,c);
+    c=a*b;
+    printf("%.2f*%.2f=%.2f\n" ,a,b,c);
+    c=a-b;
+    printf("%.2f-%.2f=%.2f\n" ,a,b,c);
+    c=a/b;
+    printf("%.2f/%.2f=%.2f\n" ,a,b,c);
+    
+}
