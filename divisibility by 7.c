57e41021f35a03a187ae0284b1fd26ee7479bed5
Devarsh Shah <devarshshah282007@gmail.com>
2025-09-08 12:28:52 +0530

    Create divisibility by 7.c

---
+++ divisibility by 7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a number\n:");
+    scanf("%d",&a);
+
+    if(a%7==0)
+    {
+    printf("%d is divisible by 7 \n" ,a);
+    }
+ else
+ {
+ printf("%d is not divisible by 7 \n" ,a);
+
+ }
+}
