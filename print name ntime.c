0a27b2e58c8d7d18f2f2a18e67fbc6cb81704827
Devarsh Shah <devarshshah282007@gmail.com>
2025-09-08 15:55:28 +0530

    Create print name ntime.c

---
+++ print name ntime.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ int main() 
+{
+    int n,i;
+    printf("How many times you want to print your name\n:");
+    scanf("%d" ,&n);
+    for(i=1;i<=n; i++)
+    {
+    printf("Devarsh \n" ,n);
+    }
+    return 0;
+    }
