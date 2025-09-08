00fc2f0b303eaef61df58501e875675c83b1c1fb
Devarsh Shah <devarshshah282007@gmail.com>
2025-09-08 15:27:31 +0530

    Create sum of 1st n even no.c

---
+++ sum of 1st n even no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int main() 
+{
+    int n,i,a=0;
+    printf("Enter a number\n :");
+    scanf("%d",&n);
+    for(i=2;i<=n;i=i+2)
+    {
+       a=a+i;
+    }
+    printf("%d\n",a);
+    }
+   
