c3c6b4fc61b0490921766ed9e6d34cb89a7f13fa
Devarsh Shah <devarshshah282007@gmail.com>
2025-10-12 12:35:19 +0530

    Update sum of 1st n even no.c

--- sum of 1st n even no.c
+++ sum of 1st n even no.c
@@ -5,9 +5,9 @@
     int n,i,a=0;
     printf("Enter a number\n :");
     scanf("%d",&n);
-    for(i=2;i<=n;i=i+2)
+    for(i=2;i<=n;i++)
     {
-       a=a+i;
+       a=a+2*i;
     }
     printf("%d\n",a);
     }
